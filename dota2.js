68ffdbf9cc0edfd7f3f30aad16b1b041646ba8fb
Christian Stewart <kidovate@gmail.com>
2014-03-07 19:54:58 -0800

    Initial

---
+++ dota2.js
@@ -0,0 +1,9 @@
+function Dota2(apikey){
+  this.apikey = apikey;
+}
+
+Dota2.prototype.matchHistory = function(filters){
+  filters["key"] = this.apikey;
+  var url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/";
+  return HTTP.get(url, {data: filters}).data;
+}
---
+++ package.js
@@ -0,0 +1,9 @@
+Package.describe({
+  summary: "Access to the API for Dota 2."
+});
+
+Package.on_use(function (api, where) {
+  api.use("http");
+  api.add_files('dota2.js', ['client', 'server']);
+  api.export("Dota2");
+});
