9d25efc0b0d25b8f01f769608f3582ebc8e3094a
Christian Stewart <kidovate@gmail.com>
2014-03-07 20:28:37 -0800

    Remove console.log

--- dota2.js
+++ dota2.js
@@ -5,7 +5,6 @@ Dota2 = function(apikey){
 Dota2.prototype.matchHistory = function(filters){
   if(filters == undefined){ filters = {}; }
   filters["key"] = this.apikey;
-  console.log(filters);
   var url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/";
   return HTTP.get(url, {params: filters}).data.result;
 }
